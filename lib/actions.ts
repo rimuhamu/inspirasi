b4db16be7a31d6ff34d6588dec5cf88686b7c754
Emir Risyad <emirrsyd@gmail.com>
2023-06-26 00:40:48 +0200

    added google login button

---
+++ lib/actions.ts
@@ -0,0 +1,12 @@
+import { GraphQLClient } from "graphql-request";
+
+const isProduction = process.env.NODE_ENV === "production";
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_URL || '':
+const client = new GraphQLClient("apiUrl");
+
+const makeGraphQLRequest = async (query: string, variables = {}) => {
+  try {
+  } catch (error) {
+    throw error;
+  }
+};
